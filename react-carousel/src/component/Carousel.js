89c4e72af101cbc1c3871ba6740bf4f5b178163e
willzhiwang <willzhiwang@gmail.com>
2020-01-09 02:14:51 -0600

    update hooks

--- react-carousel/src/component/Carousel.js
+++ react-carousel/src/component/Carousel.js
@@ -1,71 +1,64 @@
-import React, { Component } from 'react';
-import ChangePhoto from './ChangePhoto';
-import axios from 'axios'
-export default class Carousel extends Component {
-    state = {
-        photos: [],
-        index: 1
-    }
-    componentDidMount = () => {
-        fetch(`https://jsonplaceholder.typicode.com/photos`)
-            .then(response => response.json())
-            .then(
-                data => {
-                    for (let i = 0; i < 5; i++) {
-                        this.setState({
-                            photos: [...this.state.photos, data[i].url]
-                        })
-                    }
-                }
-            ).then(() => {
-                console.log(this.state.photos[0])
-            });
+import React, { useState, useEffect } from "react";
+import ChangePhoto from "./ChangePhoto";
+import axios from "axios";
+const Carousel = () => {
+  const [photos, setPhotos] = useState([]);
+  const [index, setIndex] = useState(1);
 
-        // for (let i = 1; i < 6; i++) {
-        //     let response = await axios(`https://jsonplaceholder.typicode.com/photos/${i}`)
-        //     this.setState({
-        //         photos: [...this.state.photos, response.data.url]
-        //     })
-        // }
-    }
-    minusHandler = (e) => {
-        //console.log(this.state.index);
-        if (this.state.index > 0) {
-            this.setState({
-                index: this.state.index - 1
-            })
-            console.log(e);
-        }
-    }
-    plusHandler = (e) => {
-        if (this.state.index < 4) {
-            this.setState({
-                index: this.state.index + 1
-            })
-            console.log(e);
-        }
-    }
-    searchHandler = (e) => {
-        if (e.key === 'Enter') {
-            if (e.target.value >= 0 && e.target.value <= 4) {
-                this.setState({
-                    index: e.target.value
-                })
-            }
-        }
+  useEffect(() => {
+    getPhotos();
+  }, []);
 
+  const getPhotos = async () => {
+    // fetch
+    // fetch(`https://jsonplaceholder.typicode.com/photos`)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     for (let i = 0; i < 5; i++) {
+    //       console.log(data[i].url);
+    //       setPhotos(photos => [...photos, data[i].url]);
+    //     }
+    //   })
+    //   .then(() => {
+    //     //console.log(photos);
+    //   });
+
+    // axios
+    for (let i = 1; i < 6; i++) {
+      let response = await axios(
+        `https://jsonplaceholder.typicode.com/photos/${i}`
+      );
+      setPhotos(photos => [...photos, response.data.url]);
     }
-    render() {
-        const { photos, index } = this.state
-        return (
-            <div>
-                <img src={photos[index]} alt='dd'></img>
-                <ChangePhoto
-                    minus={this.minusHandler}
-                    plus={this.plusHandler}
-                    search={this.searchHandler}
-                />
-            </div>
-        )
+  };
+  const minusHandler = e => {
+    if (index > 0) {
+      setIndex(index - 1);
     }
-}
+  };
+  const plusHandler = e => {
+    if (index < 4) {
+      setIndex(index + 1);
+    }
+  };
+  const searchHandler = e => {
+    if (e.key === "Enter") {
+      if (e.target.value >= 0 && e.target.value <= 4) {
+        setIndex(e.target.value);
+      }
+    }
+  };
+  console.log(photos, index);
+  return (
+    <div>
+      <img src={photos[index]} alt="dd"></img>
+      <ChangePhoto
+        minus={minusHandler}
+        plus={plusHandler}
+        search={searchHandler}
+      />
+    </div>
+  );
+};
+
+export default Carousel;
