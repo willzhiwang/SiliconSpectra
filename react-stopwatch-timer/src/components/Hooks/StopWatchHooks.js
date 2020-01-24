4037209ca3a248c06a09cf78b8cec2558c2389ad
willzhiwang <willzhiwang@gmail.com>
2020-01-24 11:18:42 -0600

    Update StopWatchHooks.js

--- react-stopwatch-timer/src/components/Hooks/StopWatchHooks.js
+++ react-stopwatch-timer/src/components/Hooks/StopWatchHooks.js
@@ -1,60 +1,59 @@
-import React, {useState} from "react";
-
+import React, { useState } from "react";
 
 const StopWatchHooks = () => {
   const [lapse, setLapse] = useState(0);
   const [running, setRunning] = useState(false);
+  const [timer, setTimer] = useState();
 
-  const handleWatch = ()=>{
-    let timer;
+  const handleWatch = timer => {
     if (running) {
       clearInterval(timer);
     } else {
       const startTime = Date.now() - lapse;
-       timer = setInterval(() => {
-        setLapse(Date.now() - startTime
-        );
+      setTimer(() => {
+        return setInterval(() => {
+          setLapse(Date.now() - startTime);
+        });
       });
     }
-    setRunning;
-  }
+    setRunning(!running);
+  };
 
-  const lapseFormat=()=>{
+  const lapseFormat = () => {
     let time;
-    if((lapse/1000)> 3600){
-      const hour = ~~(lapse/3600000);
-      const min_second = lapse-3600000;
-      const min = ~~(min_second/60000);
-      const second = ((lapse%60000)/1000).toFixed(2);
+    if (lapse / 1000 > 3600) {
+      const hour = ~~(lapse / 3600000);
+      const min_second = lapse - 3600000;
+      const min = ~~(min_second / 60000);
+      const second = ((lapse % 60000) / 1000).toFixed(2);
       time = `${hour}h ${min}m ${second}`;
+    } else if (lapse > 60000 && lapse < 3600000) {
+      console.log(~~(lapse / 60000.0));
+      const min = ~~(lapse / 60000.0);
+      const second = ((lapse % 60000.0) / 1000.0).toFixed(2);
+      time = `${min}m ${second}`;
+    } else {
+      time = (lapse / 1000).toFixed(2);
     }
-    else if(lapse>60000 && lapse<3600000){
-      console.log(~~(lapse/60000.00))
-      const min = ~~(lapse/60000.00);
-      const second = ((lapse%60000.00)/1000.00).toFixed(2);
-       time = `${min}m ${second}`;
-    }
-    else{
-       time = (lapse/1000).toFixed(2);
-    }
-    
+
     return time;
-  }
-  const handleClearClick =()=>{
+  };
+  const handleClearClick = () => {
+    clearInterval(timer);
     setLapse(0);
     setRunning(false);
-  }
+  };
   return (
     <div className="container">
-        <h4>StopWatch:</h4>
-        <div className="time">{lapseFormat(lapse)} s</div>
-        <button onClick={e => handleWatch()}>
-          {running ? "Stop" : "Start"}
-        </button>
-        <button className="clear" onClick={e => handleClearClick()}>
-          Reset
-        </button>
-      </div>
+      <h4>StopWatch:</h4>
+      <div className="time">{lapseFormat(lapse)} s</div>
+      <button onClick={e => handleWatch(timer)}>
+        {running ? "Stop" : "Start"}
+      </button>
+      <button className="clear" onClick={e => handleClearClick()}>
+        Reset
+      </button>
+    </div>
   );
 };
 
