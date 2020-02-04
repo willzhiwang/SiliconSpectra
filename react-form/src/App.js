2b7e4434fab3982f0950f52fd18f77c562b32f84
Zhi Wang <willzhiwang@gmail.com>
2020-02-04 03:56:58 -0600

    form practice

---
+++ react-form/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import FormClass from "./components/FormClass";
+
+function App() {
+  return (
+    <div className="App">
+      <FormClass />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-form/src/components/FormClass.js
@@ -0,0 +1,86 @@
+import React, { Component, Fragment } from "react";
+
+class FormClass extends Component {
+  state = {
+    code: "001-",
+    phone: "",
+    email: "",
+    context: [],
+    error: false
+  };
+
+  chooseCode = e => {
+    this.setState({ code: e.target.value });
+  };
+
+  handlePhoneNum = e => {
+    let number = e.target.value.substring(4);
+    console.log(number);
+
+    this.setState({ phone: number });
+  };
+
+  handleEmailChange = e => {
+    this.setState({ email: e.target.value });
+  };
+  checkEmail = () => {
+    if (this.state.email.includes("@")) {
+      const newContext = { phone: this.state.phone, email: this.state.email };
+      console.log(newContext);
+      this.setState(state => ({
+        error: false,
+        phone: "",
+        email: "",
+        context: [...this.state.context, newContext]
+      }));
+    } else {
+      this.setState(state => ({
+        error: true
+      }));
+    }
+  };
+  render() {
+    let details;
+    let context = this.state.context;
+    if (this.state.error) {
+      details = <p>The email address is invalid</p>;
+    } else {
+      details = (
+        <div>
+          {this.state.context.map((c, i) => (
+            <div key={i}>
+              <p>{c.phone}</p>
+              <p>{c.email}</p>
+            </div>
+          ))}
+        </div>
+      );
+    }
+    console.log(this.state.context);
+
+    return (
+      <div>
+        <select onChange={this.chooseCode}>
+          <option value={"001-"}>CHINA</option>
+          <option value={"002-"}>USA</option>
+          <option value={"003-"}>JAPAN</option>
+        </select>
+        <input
+          value={this.state.code + this.state.phone}
+          onChange={this.handlePhoneNum}
+          placeholder={"phone number"}
+        />
+        <input
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+          placeholder={"email address"}
+          type="email"
+        />
+        <button onClick={this.checkEmail}>SUBMIT</button>
+        {details}
+      </div>
+    );
+  }
+}
+
+export default FormClass;
