68a8579f3af2054699c6058da999762c2c65c9c4
Zhi Wang <willzhiwang@gmail.com>
2020-02-03 21:15:38 -0600

    code

--- js-CodingQuestions/higherestFrequencyElement.js
+++ js-CodingQuestions/higherestFrequencyElement.js
@@ -1,5 +1,25 @@
-let str = "abcdefghijka";
+/**
+ * js 统计一个字符串频率最高的字母
+ */
+let str = "abcdefghijkbbb";
+//console.log(str);
+
 const strChar = str => {
-  max = 0;
+  let max = 0;
+  let obj = {};
+  let maxElement = "";
+  let strArr = [...str];
+  strArr.forEach(element => {
+    if (obj[element] == undefined) {
+      obj[element] = 1;
+    } else {
+      obj[element]++;
+    }
+    if (obj[element] > max) {
+      max = obj[element];
+      maxElement = element;
+    }
+  });
+  return maxElement;
 };
-console.log(str);
+console.log(strChar(str));
