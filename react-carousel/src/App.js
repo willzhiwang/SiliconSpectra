4e2044c86bd0e5d77d90263dc3bb5cd31689405c
Zhi Wang <willzhiwang@gmail.com>
2020-02-04 03:58:17 -0600

    carousel

--- react-carousel/src/App.js
+++ react-carousel/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 // import logo from './logo.svg';
 // import './App.css';
-import CarouselHooks from "./component/CarouselHooks";
-import CarouselClass from "./component/CarouselClass";
+import CarouselHooks from "./components/CarouselHooks";
+import CarouselClass from "./components/CarouselClass";
 
 function App() {
   return (
