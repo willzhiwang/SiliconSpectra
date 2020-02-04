dff1e2f0f2a7b0fcd2cb2c6ea4af40c7c8e0e8d3
willzhiwang <willzhiwang@gmail.com>
2020-02-04 05:52:25 -0600

    coding questions

--- js-CodingQuestions/removeDuplicates.js
+++ js-CodingQuestions/removeDuplicates.js
@@ -5,3 +5,11 @@
 let arr = ["1", "2", "3", "1", "a", "b", "b"];
 
 // forEach
+
+//Filter
+const uniqueFilter = arr => {
+  return arr.filter((element, index, array) => {
+    return index === array.indexOf(element);
+  });
+};
+console.log(uniqueFilter(arr));
