051d2a97700c27d2ab8ad2ac9772ed31f917e022
Zhi Wang <willzhiwang@gmail.com>
2020-02-08 02:26:34 -0600

    sort todos types

--- Vue/vue-todolist/src/components/TodoList/TodoList.vue
+++ Vue/vue-todolist/src/components/TodoList/TodoList.vue
@@ -2,7 +2,16 @@
   <div class="hello">
     <h1>{{ title }}</h1>
     <AddTodo v-on:add-todo="addTodo" />
-    <Todos :todos="todos" v-on:del-todo="deleteTodo" />
+    <div>
+      <button class="btn" @click="displayType('a')">All</button>
+      <button class="btn" @click="displayType('p')">Progressing</button>
+      <button class="btn" @click="displayType('d')">Done</button>
+    </div>
+    <Todos
+      :todos="todos"
+      v-on:edit-todo="editTodo"
+      v-on:del-todo="deleteTodo"
+    />
   </div>
 </template>
 
@@ -15,10 +24,21 @@ export default {
   name: "TodoList",
   components: { Todos, AddTodo },
   data() {
-    return { title: "Todo List", todos: [] };
+    return { title: "Todo List", todos: [], type: "All" };
   },
   methods: {
+    editTodo(todo) {
+      console.log(todo.title);
+      const { id, title, completed } = todo;
+      axios
+        .put(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+          title,
+          completed
+        })
+        .then(res => console.log("res: ", res));
+    },
     deleteTodo(id) {
+      console.log(id);
       axios
         .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
         .then(res => {
@@ -28,6 +48,7 @@ export default {
         .catch(err => console.log(err));
     },
     addTodo(newTodo) {
+      console.log(newTodo);
       const { title, completed } = newTodo;
       axios
         .post("https://jsonplaceholder.typicode.com/todos", {
@@ -36,6 +57,18 @@ export default {
         })
         .then(res => (this.todos = [...this.todos, res.data]))
         .catch(err => console.log(err));
+    },
+    displayType(e) {
+      if (e == "a") {
+        this.type = "All";
+      }
+      if (e == "p") {
+        this.type = "Progress";
+      }
+      if (e == "d") {
+        this.type = "Done";
+      }
+      console.log(this.type);
     }
   },
   created() {
@@ -49,7 +82,12 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -63,5 +101,17 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
