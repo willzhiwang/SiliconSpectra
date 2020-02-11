d027e2563bd0d93d82b77b8b73976276e9a71039
Zhi Wang <willzhiwang@gmail.com>
2020-02-11 17:11:26 -0600

    Upadte commit variable names

--- Vue/vue-todolist-vuex/src/store/modules/Todos.js
+++ Vue/vue-todolist-vuex/src/store/modules/Todos.js
@@ -14,21 +14,21 @@ const actions = {
       `https://jsonplaceholder.typicode.com/todos?_limit=5`
     );
     //console.log(res.data);
-    commit("setTodos", res.data);
+    commit("Set_To_Dos", res.data);
   },
   async addTodo({ commit }, title) {
     const res = await axios.post(`https://jsonplaceholder.typicode.com/todos`, {
       title,
       completed: false
     });
-    commit("newTodo", res.data);
+    commit("New_To_Do", res.data);
     console.log(state.todos);
   },
   async delTodo({ commit }, id) {
     const res = await axios.delete(
       `https://jsonplaceholder.typicode.com/todos/${id}`
     );
-    commit("removeTodo", id);
+    commit("Remove_To_Do", id);
   },
   async updateTodo({ commit }, todo) {
     const res = await axios.put(
@@ -40,15 +40,15 @@ const actions = {
     console.log(res.data);
     console.log(state.todos);
 
-    commit("changeTodo", todo);
+    commit("Change_To_Do", todo);
   }
 };
 const mutations = {
-  setTodos: (state, todos) => (state.todos = todos),
-  newTodo: (state, todo) => state.todos.unshift(todo),
-  removeTodo: (state, id) =>
+  Set_To_Dos: (state, todos) => (state.todos = todos),
+  New_To_Do: (state, todo) => state.todos.unshift(todo),
+  Remove_To_Do: (state, id) =>
     (state.todos = state.todos.filter(todo => todo.id !== id)),
-  changeTodo: (state, updTodo) => {
+  Change_To_Do: (state, updTodo) => {
     console.log(state.todos);
     const index = state.todos.findIndex(todo => todo.id === updTodo.id);
     if (index !== -1) {
